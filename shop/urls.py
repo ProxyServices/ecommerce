27b65b82f23df4bf965a2f0e7cbeba253f418d66
EnockOMONDI <enockomondike@gmail.com>
2019-08-01 19:27:09 +0300

    first commit

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import re_path
+from . import views
+
+urlpatterns = [
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^about/', views.about, name='about'),
+    re_path(r'^shop/', views.product_list, name='product_list'),
+    re_path(r'^contact/', views.contact, name='contact'),
+    re_path(r'^media/', views.media_list, name='media_list'),
+    re_path(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
+    re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+]
