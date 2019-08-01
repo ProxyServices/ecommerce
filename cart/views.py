43858b8305f3c19207da6d0b25f9dcfbba0f2c23
EnockOMONDI <enockomondike@gmail.com>
2019-08-01 19:53:18 +0300

    first commit

--- cart/views.py
+++ cart/views.py
@@ -30,7 +30,7 @@ def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    return render(request, 'cart/detail.html', {'cart': Cart})
     
 @csrf_exempt
 def sms_response(request):
